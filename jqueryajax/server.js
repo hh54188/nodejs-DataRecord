538c49ce69f7ef14f43e70343fb6db5c286395a4
lee gy <hh54188@yahoo.cn>
2012-07-26 00:16:35 +0800

    still not find static file

--- jqueryajax/server.js
+++ jqueryajax/server.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var app = express.createServer();
+
+app.use(express.logger());
+app.use(app.router);
+app.use(express.bodyParser());
+
+
 app.get('/', function(req, res){
     res.send('Hello World');
 });
+
+app.post('/', function(req, res){
+  res.send(req.body);
+});
+
+
+var callback = function (req, res, next) {
+	var user = req.params.id;
+	if (user) {
+		req.call = user;
+		next();
+	}
+}
+
+app.get('/user/:id', callback, function(req, res){
+    res.send('what in call back is: ' + req.call);
+});
+
+
 
 app.listen(8000);
