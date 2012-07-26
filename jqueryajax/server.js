dc30fe504a6f52ee4818164fd86e2a2ebc2facb1
lee gy <hh54188@yahoo.cn>
2012-07-26 08:34:16 +0800

    static file success

--- jqueryajax/server.js
+++ jqueryajax/server.js
@@ -1,31 +1,34 @@
 var express = require('express');
 var app = express.createServer();
+app.configure(function () {
+	app.use(express.static(__dirname + '/static'));
+})
 
-app.use(express.logger());
-app.use(app.router);
-app.use(express.bodyParser());
+// app.use(express.logger());
+// app.use(app.router);
+// app.use(express.bodyParser());
 
 
 app.get('/', function(req, res){
     res.send('Hello World');
 });
 
-app.post('/', function(req, res){
-  res.send(req.body);
-});
+// app.post('/', function(req, res){
+//   res.send(req.body);
+// });
 
 
-var callback = function (req, res, next) {
-	var user = req.params.id;
-	if (user) {
-		req.call = user;
-		next();
-	}
-}
+// var callback = function (req, res, next) {
+// 	var user = req.params.id;
+// 	if (user) {
+// 		req.call = user;
+// 		next();
+// 	}
+// }
 
-app.get('/user/:id', callback, function(req, res){
-    res.send('what in call back is: ' + req.call);
-});
+// app.get('/user/:id', callback, function(req, res){
+//     res.send('what in call back is: ' + req.call);
+// });
 
 
 
