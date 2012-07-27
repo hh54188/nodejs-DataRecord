72d3b59618a1ea815950242c5ea9d5044bf07228
LiGuangyi <hh54188@yahoo.cn>
2012-07-27 18:34:45 +0800

    it is the order problem

--- jqueryajax/server.js
+++ jqueryajax/server.js
@@ -3,8 +3,9 @@ var app = express.createServer();
 app.configure(function () {
 	app.use(express.static(__dirname + '/static'));
 	// app.use(express.logger());
-	// app.use(app.router);
 	app.use(express.bodyParser());
+	app.use(app.router);
+
 })
 
 
