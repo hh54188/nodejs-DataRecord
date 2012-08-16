dce219963bfed798520087c692af38b778f1fe37
LiGuangyi <hh54188@yahoo.cn>
2012-08-16 18:38:32 +0800

    install express

---
+++ appletree/server/client.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+app.configure(function () {
+	app.use(express.static(__dirname + '/static'));
+	app.use(express.bodyParser());
+	app.use(app.router);
+
+})
+app.get('/', function (req, res) {
+	res.send('hello world');
+});
+app.listen(9000);
